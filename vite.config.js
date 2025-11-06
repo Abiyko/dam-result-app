9d8048dcea1bb4d345345f68bd3e14d5db1fba05
Abiyko <abiyko.syumi002@gmail.com>
2025-11-06 12:03:23 +0900

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -2,5 +2,13 @@
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-  base: '/',
+  // 以前ご指摘した基本設定
+  base: '/', 
+  
+  resolve: {
+    // node_modulesの依存関係を明示的に解決するように指示
+    alias: {
+      '@auth0/auth0-spa-js': '@auth0/auth0-spa-js',
+    },
+  },
 });
